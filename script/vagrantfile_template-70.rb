afa97200a2ea68a1cab9d99371e264c1c80ccc35
Mike Barker <mike@thebarkers.com>
2021-02-07 10:41:36 -0700

    Add files to build a esxi v7.0 packer box

---
+++ script/vagrantfile_template-70.rb
@@ -0,0 +1,29 @@
+Vagrant.require_version '>= 1.6.2'
+
+Vagrant.configure('2') do |config|
+  # We're using the root user here.
+  config.ssh.username = 'root'
+  # Set default shell to SH.
+  config.ssh.shell = 'sh'
+
+  # Vagrant doesn't support insert key on ESXi so let's stick with the unsecure
+  # key for now.
+  config.ssh.insert_key = false
+
+  # Do not sync default /vagrant folder on ESXi.
+  config.vm.synced_folder '.', '/vagrant', disabled: true
+
+  # We don't have NFS working inside ESXi so we flag this just in case.
+  config.nfs.functional = false
+
+  config.vm.provider "vmware_desktop" do |v|
+    # Use paravirtualized virtual hardware on VMW hypervisors.
+    v.vmx['ethernet0.virtualDev'] = 'vmxnet3'
+    v.vmx['memsize'] = '4096'
+    v.vmx['numvcpus'] = '2'
+    # ESXi 7+ need para virtualized scsi adapters
+    v.vmx["scsi0.virtualDev"] = "pvscsi"
+    # Enable nested virtualization.
+    v.vmx['vhv.enable'] = 'true'
+  end
+end
